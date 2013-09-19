4733b14674647c568f0faf1385ef8e96f785fb0a
Kevin Ryan <james.ryan@uconn.edu>
2013-09-19 12:41:59 -0400

    Updated sample_app with the appropriate title insertion code.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -28,7 +28,17 @@ describe "Static pages" do
 		end
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us");
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
+	describe "Contact Page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
 		end
 	end
 end
