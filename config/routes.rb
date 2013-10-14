8f45a2f9b011475ac2a67dbf2e408621f3cb9b82
Kevin Ryan <james.ryan@uconn.edu>
2013-10-14 10:32:30 -0400

    Added product model and view to web server as per assignment 5 instructions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  
+	get "product/new"
+
+	get "users/new"
 	get "users/new"
 
 	root 'static_pages#home'
@@ -10,8 +13,9 @@ SampleApp::Application.routes.draw do
 	match('/terms', {to: 'static_pages#terms', via: 'get'})
  
 	resources :users
- 
-# The priority is based upon order of creation: first created -> highest priority.
+  	resources :products
+
+  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
