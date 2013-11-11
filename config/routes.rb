fe2a989801ef48deed735cbcd1575cb767142fc6
Kevin Ryan <james.ryan@uconn.edu>
2013-11-11 09:43:40 -0500

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ SampleApp::Application.routes.draw do
 	resources :users
   	resources :products
 	resources :sessions, only: [:new, :create, :destroy]
+	resources :microposts, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
